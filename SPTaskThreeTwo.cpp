2586de83c2b1153759f231ea76d06fda7d1f6a02
Wyman <denisgorbatuk57@gmail.com>
2024-02-03 09:45:00 +0500

    Initial commit

---
+++ SPTaskThreeTwo.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+#define e 2.71828182845904523536
+#define x 2.87
+#define y 0.84
+#define z ( 2 * x * y ) / ( x + cos( y ) )
+#define b sqrt( x * sin( 2 ) * y ) / ( z + pow( e , x ) * y )
+
+int main()
+{
+	cout << "Const e = " << e << endl << "x = " << x << endl << "y = " << y << endl << "z = " << z << endl << "b = " << b;
+}
